95fff67ce7ab3a1afa1951d44914b84b740e7414
namingDiff <30595707+namingDiff@users.noreply.github.com>
2019-07-25 17:30:36 +0800

    Update 计算一年中第几周的时间范围.js

--- js/计算一年中第几周的时间范围.js
+++ js/计算一年中第几周的时间范围.js
@@ -1,20 +1,20 @@
-var year = 2019 //选择年份
+    var year = 2019 //选择年份
     var lastDay = new Date(year + 1,0,0) // 一年的最后一天
     var firstDay = new Date(year,0,1) // 一年的最后一天
     var obj = {}
     var index = 1
-    var day = new Date(year,0,1)
+    
     function formatDate(d){
         return d.toLocaleDateString('zh-cn',{year: 'numeric', month: '2-digit',day: '2-digit'}).replace(/\//g,'-')
     }
-    while (day < lastDay){
-        day.setDate(day.getDate() + 1)
-        if(day.getDay() === 6){
-            obj['第' + (index++) + '周'] = formatDate(firstDay) + ' 到 ' + formatDate(day)
-            firstDay = new Date(day.getTime() + 86400000)
+    while (firstDay < lastDay){
+        firstDay.setDate(firstDay.getDate() + 1)
+        if(firstDay.getDay() === 6){
+            obj[index++] = formatDate(firstDay) + ' - ' + formatDate(firstDay)
+            firstDay = new Date(firstDay.getTime() + 86400000)
         }
     }
-    if(day !== lastDay){
-        obj['第' + (index++) + '周'] = formatDate(firstDay) + ' 到 ' + formatDate(lastDay)
+    if(firstDay !== lastDay){
+        obj[index++] = formatDate(firstDay) + ' - ' + formatDate(lastDay)
     }
     console.log(JSON.stringify(obj,null,' '))
